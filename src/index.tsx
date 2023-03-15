27e0796c1eaad9b4541a416433f8f31c66a811a7
Alexander Voronin <aovoronin.piano@gmail.com>
2023-03-15 10:53:00 +0300

    feat: remove material ui, check tests and eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 import { router } from './router/router';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
