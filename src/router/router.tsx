1cdbb2fd8f8e1e54cfdf301dfed39bea99c70743
Alexander Voronin <aovoronin.piano@gmail.com>
2023-03-24 09:19:38 +0300

    fix: fix conflicts between branches

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,8 +2,8 @@ import App from '../pages/main/main';
 import { createBrowserRouter } from 'react-router-dom';
 import React from 'react';
 import Forms from 'src/pages/forms/forms';
-import NotFoundPage from 'src/pages/not-found-page/not-found-page';
 import AboutUs from 'src/pages/about-us/about-us';
+import ErrorPage from 'src/pages/error-page/error-page';
 
 export const router = createBrowserRouter([
   {
@@ -18,6 +18,6 @@ export const router = createBrowserRouter([
   {
     path: '/forms',
     element: <Forms />,
-    errorElement: <NotFoundPage />,
+    errorElement: <ErrorPage />,
   },
 ]);
