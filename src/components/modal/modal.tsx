1676d44272344cd12a3f55cd5919745b4f96be1a
Alexander Voronin <aovoronin.piano@gmail.com>
2023-03-30 23:07:16 +0300

    feat: wrote a couple of tests and add modal styles

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,6 +1,7 @@
 import { UsersType } from 'src/utils/types/types';
 import React, { useEffect } from 'react';
 import setEscListener from 'src/utils/utils';
+import styles from './modal.module.scss';
 
 interface ModalProps {
   users: UsersType[];
@@ -16,24 +17,30 @@ export default function Modal({ users, idUser, setIsShowMore }: ModalProps) {
       phone,
     },
   ] = users.filter(({ id }) => id === Number(idUser));
+  const { overlay, modal } = styles;
 
   useEffect(() => {
     setEscListener(setIsShowMore);
   }, [setIsShowMore]);
 
   return (
-    <div className="user__card card" key={`${city}${zipcode}`}>
-      <div className="card__email">{email}</div>
-      <div className="card__address">
-        <div className="card__address--city">{`City: ${city}`}</div>
-        <div className="card__address--street">{`Street: ${street}`}</div>
-        <div className="card__address--suite">{`Flat: ${suite}`}</div>
-        <div className="card__address--zipcode">{`Zipcode: ${zipcode}`}</div>
-        <div className="card__address--phone">{`Phone: ${phone}`}</div>
-      </div>
-      <button type="button" onClick={() => setIsShowMore(false)}>
-        X
-      </button>
-    </div>
+    <>
+      <section className={modal}>
+        <div className="user__card card">
+          <div className="card__email">{email}</div>
+          <div className="card__address">
+            <div className="card__address--city">{`City: ${city}`}</div>
+            <div className="card__address--street">{`Street: ${street}`}</div>
+            <div className="card__address--suite">{`Flat: ${suite}`}</div>
+            <div className="card__address--zipcode">{`Zipcode: ${zipcode}`}</div>
+            <div className="card__address--phone">{`Phone: ${phone}`}</div>
+          </div>
+          <button type="button" onClick={() => setIsShowMore(false)}>
+            ⨉
+          </button>
+        </div>
+      </section>
+      <div className={overlay} onClick={() => setIsShowMore(false)}></div>
+    </>
   );
 }
