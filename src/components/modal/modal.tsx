2d5634d5e8dbfc1744cdc43fd5f28b28d4fde783
Alexander Voronin <aovoronin.piano@gmail.com>
2023-04-04 22:47:38 +0300

    feat: add scroll lock

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,5 +1,7 @@
 import { UsersType } from 'src/utils/types/types';
 import React, { useEffect } from 'react';
+import { RemoveScroll } from 'react-remove-scroll';
+import FocusLock from 'react-focus-lock';
 import setEscListener from 'src/utils/utils';
 import styles from './modal.module.scss';
 
@@ -26,19 +28,23 @@ export default function Modal({ users, idUser, setIsShowMore }: ModalProps) {
   return (
     <>
       <section className={modal}>
-        <div className="user__card card">
-          <div className="card__email">{email}</div>
-          <div className="card__address">
-            <div className="card__address--city">{`City: ${city}`}</div>
-            <div className="card__address--street">{`Street: ${street}`}</div>
-            <div className="card__address--suite">{`Flat: ${suite}`}</div>
-            <div className="card__address--zipcode">{`Zipcode: ${zipcode}`}</div>
-            <div className="card__address--phone">{`Phone: ${phone}`}</div>
-          </div>
-          <button type="button" onClick={() => setIsShowMore(false)}>
-            ⨉
-          </button>
-        </div>
+        <RemoveScroll>
+          <FocusLock>
+            <div className="user__card card">
+              <div className="card__email">{email}</div>
+              <div className="card__address">
+                <div className="card__address--city">{`City: ${city}`}</div>
+                <div className="card__address--street">{`Street: ${street}`}</div>
+                <div className="card__address--suite">{`Flat: ${suite}`}</div>
+                <div className="card__address--zipcode">{`Zipcode: ${zipcode}`}</div>
+                <div className="card__address--phone">{`Phone: ${phone}`}</div>
+              </div>
+              <button type="button" onClick={() => setIsShowMore(false)}>
+                ⨉
+              </button>
+            </div>
+          </FocusLock>
+        </RemoveScroll>
       </section>
       <div className={overlay} onClick={() => setIsShowMore(false)}></div>
     </>
