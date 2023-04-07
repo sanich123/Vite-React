9b2021de34d211592c36b0bd1efb6369922746af
sanich123 <aovoronin.piano@gmail.com>
2023-04-07 10:47:28 +0300

    fix: some work with tests

--- src/components/dates/date-inputs.tsx
+++ src/components/dates/date-inputs.tsx
@@ -5,13 +5,16 @@ import { InputRegisterType } from 'src/utils/types/form-types';
 export default function DateInputs({ register, errors }: InputRegisterType) {
   return (
     <>
-      {DATE_MOCKS.map(({ name, min, max, title, type }) => (
-        <div key={`${name}${title}`} className={`input__wrapper input-${name}`}>
-          <label htmlFor={`input-${name}`}>{title}</label>
-          <input {...register(name, { required: true })} type={type} name={name} id={`input-${name}`} min={min} max={max} className="input__date" />
-          {errors[name] && <span className="errors-span">This field is required</span>}
-        </div>
-      ))}
+      {DATE_MOCKS.map(({ name, min, max, title, type }) => {
+        console.log(errors);
+        return (
+          <div key={`${name}${title}`} className={`input__wrapper input-${name}`}>
+            <label htmlFor={`input-${name}`}>{title}</label>
+            <input {...register(name, { required: true })} type={type} name={name} id={`input-${name}`} min={min} max={max} className="input__date" />
+            {errors[name] && <span className="errors-span">This field is required</span>}
+          </div>
+        );
+      })}
     </>
   );
 }
