09cece5901e2a70095f2549371f696093649d225
Alexander Voronin <aovoronin.piano@gmail.com>
2023-04-07 23:08:05 +0300

    feat: write a large complex test for forms component

--- src/pages/forms/forms.test.tsx
+++ src/pages/forms/forms.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { describe, expect, it } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { screen } from '../../tests/test-utils';
 import Forms from './forms';
 import { COUNTRIES } from 'src/utils/const/texts';
@@ -20,9 +20,11 @@ describe('Forms', () => {
     expect(screen.getAllByRole('combobox')).toHaveLength(2);
   });
   it('should correctly interract with the user', async () => {
+    global.URL.createObjectURL = vi.fn();
+    const user = userEvent.setup();
     const file = new File(['hello'], 'hello.png', { type: 'image/png' });
     renderWithProviders(<Forms />);
-    const inputName = screen.getByRole('textbox', { name: 'name' });
+    const inputName = screen.getByLabelText('Name');
     const inputSurname = screen.getByRole('textbox', { name: /surname/i });
     const inputZipcode = screen.getByRole('textbox', { name: /zipcode/i });
     const inputTime = screen.getByLabelText(/choose your delivery time:/i);
@@ -33,18 +35,21 @@ describe('Forms', () => {
     const emailCheckbox = screen.getByLabelText(/send me email/i);
     const submitBtn = screen.getByRole('button');
     const inputFile = screen.getByLabelText(/send your photo/i);
-    await userEvent.type(inputSurname, 'Alexander');
+    await user.type(inputName, 'Some value');
+    expect(await screen.findByDisplayValue(/some value/i)).toBeDefined();
+    await user.type(inputSurname, 'Alexander');
     expect(await screen.findByDisplayValue(/alexander/i)).toBeDefined();
-    await userEvent.type(inputZipcode, 'Voronin');
+    await user.type(inputZipcode, 'Voronin');
     expect(await screen.findByDisplayValue(/voronin/i)).toBeDefined();
-    await userEvent.type(inputTime, '13:58');
+    await user.type(inputTime, '13:58');
     expect(await screen.findByDisplayValue(/13:58/i)).toBeDefined();
-    await userEvent.type(inputDelivery, '2023-05-07');
+    await user.type(inputDelivery, '2023-05-07');
     expect(await screen.findByDisplayValue('2023-05-07')).toBeDefined();
-    await userEvent.type(inputBirthday, '1930-05-06');
+    await user.type(inputBirthday, '1930-05-06');
     expect(await screen.findByDisplayValue('1930-05-06')).toBeDefined();
-    await userEvent.click(homoSexRadio);
-    await userEvent.upload(inputFile, file);
+    await user.click(homoSexRadio);
+    await user.click(femaleRadio);
+    await user.upload(inputFile, file);
     if (homoSexRadio instanceof HTMLInputElement && femaleRadio instanceof HTMLInputElement && emailCheckbox instanceof HTMLInputElement && inputFile instanceof HTMLInputElement) {
       expect(homoSexRadio.checked).toBeDefined();
       expect(femaleRadio.checked).toBeDefined();
@@ -55,5 +60,13 @@ describe('Forms', () => {
         expect(inputFile.files).toHaveLength(1);
       }
     }
+    await user.click(submitBtn);
+    expect(await screen.findByText(/send me: nothing/i)).toBeDefined();
+    expect(await screen.findByText(/female and homosexual/i)).toBeDefined();
+    expect(await screen.findByText(/Deliver before 2023-05-07, at 13:58 in /i));
+    expect(await screen.findByText(/Was born at: 1930-05-06/i)).toBeDefined();
+    expect(await screen.findByText(/Zipcode: Voronin/i)).toBeDefined();
+    expect(await screen.findByText(/Some value Alexander/i)).toBeDefined();
+    expect(await screen.findByText(/Поздравляем вас, отправка формы произошла весьма успешно/i));
   });
 });
