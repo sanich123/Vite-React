6dbfa3d628c6fb871b67320d2820b1872fa22944
sanich123 <aovoronin.piano@gmail.com>
2023-04-11 18:36:17 +0300

    feat: experiments with resizing elements

--- src/components/grqphiql/graphiql.tsx
+++ src/components/grqphiql/graphiql.tsx
@@ -1,19 +1,44 @@
-import { GraphiQLProvider } from '@graphiql/react';
-import { createGraphiQLFetcher } from '@graphiql/toolkit';
-import React from 'react';
-import { QueryEditor } from '@graphiql/react';
-import '@graphiql/react/dist/style.css';
-
-const fetcher = createGraphiQLFetcher({
-  url: 'https://my.graphql.api/graphql',
-});
+import React, { useRef } from 'react';
+import './graphiql.scss';
 
 export default function GraphiQl() {
+  const parent = useRef<HTMLDivElement | null>(null);
+  const resizer = useRef<HTMLDivElement | null>(null);
+  const leftSide = useRef<HTMLDivElement | null>(null);
+  const rightSide = useRef<HTMLDivElement | null>(null);
+
+  let x = 0;
+  let y = 0;
+  let leftWidth = 0;
+  const mouseMoveHandler = function (e: MouseEvent) {
+    const dx = e.clientX - x;
+    // const dy = e.clientY - y;
+    console.log(parent.current, leftSide.current);
+    if (parent.current && leftSide.current) {
+      const newLeftWidth = ((leftWidth + dx) * 100) / parent.current.getBoundingClientRect().width;
+      console.log(newLeftWidth);
+      leftSide.current.style.width = `${newLeftWidth}%`;
+    }
+  };
   return (
-    <GraphiQLProvider fetcher={fetcher}>
-      <div className="graphiql-container">
-        <QueryEditor />
-      </div>
-    </GraphiQLProvider>
+    <div className="wrapper" ref={parent}>
+      <div ref={leftSide}>Left</div>
+      <div
+        className="resizer"
+        id="dragMe"
+        ref={resizer}
+        onMouseDown={({ clientX, clientY }) => {
+          x = clientX;
+          y = clientY;
+          if (leftSide.current instanceof HTMLDivElement) {
+            leftWidth = leftSide.current.getBoundingClientRect().width;
+          }
+          document.addEventListener('mousemove', mouseMoveHandler);
+          // document.addEventListener('mouseup', mouseUpHandler);
+        }}
+      />
+
+      <div ref={rightSide}>Right</div>
+    </div>
   );
 }
